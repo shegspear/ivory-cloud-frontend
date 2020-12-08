b287b6b8cd290ed16b9111cbe8f4396fcc7ea901
shegspear <shegspear@gmail.com>
2020-12-08 22:49:56 +0100

    Enabled state change functionality for the patient dashboard state and test state done, but two more states left

--- js/patientPage.js
+++ js/patientPage.js
@@ -15,6 +15,7 @@ const UI = (function() {
           newVisitBtn: '.new-visit-btn',
           rootHead: '#root-head',
           rootBody: '#root-body',
+          stateLink: '.state-link',
           dashBoardLink: '.dashboard-link',
           newVisitBtn: '.new-visit-btn',
           nurseStateBtn: '.nurse-state-btn',
@@ -331,11 +332,119 @@ const UI = (function() {
         `;
       };
 
+      // GENERAL  TEST STATE
+      const testState = function() {
+        document.querySelector(UISelector.rootHead).innerHTML = `
+            <!-- CODES FOR CONTAINER SEVEN TEST STATE  -->
+            <section class="cont-7 d-flex flex-row container my-5">
+                
+              <div class="cont-7-child-1 mr-3 text-center blood-test">
+                <p class="h4 font-weight-bold mt-4 blood-test">Blood Test</p>
+              </div>
+        
+              <div class="cont-7-child-2 mx-3 text-center urine-test">
+                <p class="h4 font-weight-bold mt-4 urine-test">Urine Test</p>
+              </div>
+        
+              <div class="cont-7-child-3 mx-3 text-center xray-test">
+                <p class="h4 font-weight-bold mt-4 xray-test">X-ray</p>
+              </div>
+        
+              <div class="cont-7-child-3 mx-3 text-center other-test">
+                <p class="h4 font-weight-bold mt-4 other-test">Others</p>
+              </div>
+        
+            </section>
+        `; 
+        
+        document.querySelector(UISelector.rootBody).innerHTML = null; 
+      };
+
+      // GENERAL TEST -> BLOOD TEST STATE
+      const bloodTestState = function() {
+        document.querySelector(UISelector.rootHead).innerHTML = `
+            <!-- CODES FOR CONTAINER SEVEN TEST STATE  -->
+            <section class="cont-7 d-flex flex-row container my-5">
+                
+              <div class="cont-7-child-1 mr-3 text-center blood-test active-test">
+                <p class="h4 font-weight-bold mt-4 blood-test">Blood Test</p>
+              </div>
+        
+              <div class="cont-7-child-2 mx-3 text-center urine-test">
+                <p class="h4 font-weight-bold mt-4 urine-test">Urine Test</p>
+              </div>
+        
+              <div class="cont-7-child-3 mx-3 text-center xray-test">
+                <p class="h4 font-weight-bold mt-4 xray-test">X-ray</p>
+              </div>
+        
+              <div class="cont-7-child-3 mx-3 text-center other-test">
+                <p class="h4 font-weight-bold mt-4 other-test">Others</p>
+              </div>
+        
+            </section>
+        `; 
+
+        document.querySelector(UISelector.rootBody).innerHTML = `
+            <!-- CODES FOR CONTAINER EIGHT TEST BLOOD TEST STATE  -->
+            <section class="cont-8 container my-5">
+              
+              <form class="test-form p-3">
+                <p class="h3 text-weight-bold u">
+                  Request Blood Test
+                </p>
+        
+                <div class="form-group">
+                  <label for="enter date">Enter date</label>
+                  <input type="date" class="form-control req-date-of-test" id="req-date-of-test" aria-describedby="date of test">
+                </div>
+        
+                <div class="form-group">
+                  <label for="enter time">Enter time</label>
+                  <input type="time" class="form-control req-time-of-test" id="req-time-of-test" aria-describedby="time of test">
+                </div>
+        
+                <div class="form-group">
+                  <label for="enter name">Enter patient name</label>
+                  <input type="text" class="form-control patient-name" id="patient-name" aria-describedby="patient name">
+                </div>
+        
+                <div class="form-group">
+                  <label for="enter patient reg number">Enter patient reg number</label>
+                  <input type="text" class="form-control patient-reg-num" id="patient-reg-num" aria-describedby="patient registration number">
+                </div>
+        
+                <div class="form-group">
+                  <label for="enter patient symptons">Enter patient symptons</label>
+                  <textarea class="form-control patient-sympt" name="patient-sympt" id="patient-sympt" cols="30" rows="10"></textarea>
+                </div>
+        
+                <div class="mt-5 text-center">
+                  <button class="btn btn-info submit-test-request">Request</button>
+                </div>
+        
+              </form>
+        
+            </section> 
+        `;
+      }
+
       // PUBLIC METHODS
       return{
         // function to get all class names and ids from the UI
         getUiSelectors: function() {
             return UISelector;
+        },
+
+        // function to check active link and relocate it
+        checkActiveLink: function(selector, className) {
+           document.querySelectorAll(selector).forEach(cur => {
+            
+                if(cur.classList[0] !== className) {
+                   cur.classList.remove('text-primary');
+                  // console.log(cur);
+                }
+           });
         },
 
         // This function sets up the needed in the UI DOM
@@ -359,6 +468,16 @@ const UI = (function() {
             // function to setup new visit doctor state
             const page = new PageState();
             page.change(new labSciState);
+
+           } else if (state === 'testState') {
+             // function to setup general test state
+             const page = new PageState();
+             page.change(new testState);
+
+           } else if (state === 'bloodTestState') {
+             // fucntion to setup blood test state
+             const page = new PageState();
+             page.change(new bloodTestState);
            }
         },
 
@@ -376,17 +495,43 @@ const Controller = (function(UI, Logic) {
 
       // EVENT LISTNERS
       const loadEventListners = function() {
-         // EVENT LITNER FOR THE DASH BORAD LINK
-         document.querySelector(uiSelectors.dashBoardLink).addEventListener('click', setDashBoardState);
+         // EVENT LISTNER FOR THE LINKS IN STATE NAVIGATION OPTIONS
+         document.querySelectorAll(uiSelectors.stateLink).forEach((cur) => {
+          cur.addEventListener('click', sortMyState);
+         });
+
+          // HAD TOO SET UP THIS EVENT LISTNER IN THE ROOT HEAD, CAUSE OF BUGS and in hind site DRY
+          document.querySelector(uiSelectors.rootHead).addEventListener('click', getState);
+
          // HAD TOO SET UP THIS EVENT LISTNER IN THE ROOT BODY, CAUSE OF BUGS and in hind site DRY
          document.querySelector(uiSelectors.rootBody).addEventListener('click', getState);
+       
       };
 
-      //
+      // sort my state function
+      const sortMyState = function(e) {
+          e.preventDefault();
+          let pointClass = e.target.classList;
+          let pointId = e.target.id;
+          
+          if(pointClass.contains('dashboard-link')) {
+            ui.getMyState('initialState');
+            pointClass.add('text-primary');
+            // console.log(pointClass);
+            ui.checkActiveLink(uiSelectors.stateLink, pointClass[0]);
+
+          } else if (pointClass.contains('test-link')) {
+            ui.getMyState('testState');
+            pointClass.add('text-primary');
+            ui.checkActiveLink(uiSelectors.stateLink, pointClass[0]);
+
+          }
+      };
+
+      // get state function
       const getState = function(e) {
          let point = e.target;
-
-         console.log(point);
+        //  console.log(point);
 
          if(point.classList.contains('new-visit-btn') || point.classList.contains('nurse-state-btn')) {
             ui.getMyState('nurseState');
@@ -397,13 +542,12 @@ const Controller = (function(UI, Logic) {
          } else if (point.classList.contains('lab-state-btn')) {
             ui.getMyState('labSciState');
 
+         } else if (point.classList.contains('blood-test')) {
+            ui.getMyState('bloodTestState');
+
          }
       };
 
-      // set dash board state funtion
-      const setDashBoardState = function() {
-        ui.getMyState('initialState');
-      };
 
       // PULIC METHODS
       return{
